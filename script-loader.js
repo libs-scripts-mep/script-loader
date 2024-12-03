e24f624419f82392e681a8e82845202dad6f8804
GuilhermeMatte <guilherme.matte@inova.ind.br>
2024-12-03 15:22:08 -0300

    Remoção da consulta via API do Effective, PVI traz o código do script a ser carregado

--- script-loader.js
+++ script-loader.js
@@ -1,45 +1,87 @@
-import Effective from "./effective.js"
+import { RastPVI, RastUtil } from "../rast-pvi/rast-pvi.js"
+import FWLink from "../daq-fwlink/FWLink.js"
+import Log from "./utils-script.js"
 
 export default class TestScriptLoader {
 
     /**
-     * @param {Array} eventMap Informa ao script as o mapa de eventos a serem cumpridos
-     * @param {String} event Informa ao script o evento atual
-     * @param {String} TSpath Caminho relativo dos scripts à serem carregados em relação à ESTE arquivo
-     * 
-     * # Exemplos
-     * 
-     * ## .html
+     * @param {string[]} eventMap Informa ao script o mapa de eventos a serem cumpridos
+     * @param {string} event Informa ao script o evento atual
+     * @param {string | string[]} tsPath Caminho relativo dos scripts a serem carregados em relação à ESTE arquivo
+     * @example
      * ```html
      * <script type="module">
-     * import TestScriptLoader from "./node_modules/@libs-scripts-mep/script-loader/script-loader.js"
-     * TestScriptLoader.Init(["TF"], "TF", "../../../test_scripts/")
+     *  import TestScriptLoader from "./node_modules/@libs-scripts-mep/script-loader/script-loader.js"
+     *  TestScriptLoader.init(
+     *      ["TF"], 
+     *      "TF", 
+     *      ["../../../products/48x48/test-scripts/", "../../../products/72x72/test-scripts/", "../../../products/75x33/test-scripts/"]
+     *  )
      * </script>
      * ```
      */
-    static async Init(eventMap, event, TSpath = "../../../Produtos/") {
-        try {
-            const ERPData = await Effective.ERPDataInit()
-            console.log("ERP Data:\n", ERPData)
+    static async init(eventMap, event, tsPath = ["../../../Produtos/"]) {
+        const code = FWLink.runInstructionS("rastreamento.getproductcode", [])
 
-            const productCode = Effective.getProductCode()
+        if (code == "") {
+            Log.console("Informações do produto não estão previamente carregadas no PVI", Log.Colors.Orange.Coral)
+            await this.rastInit(eventMap, event)
+            location.reload()
+            return
+        }
 
-            if (productCode != "") {
-                const testScript = await import(`${TSpath}${productCode}.js`)
-                window.TS = new testScript.default(eventMap, event)
-            } else { throw ("Código do produto baseado no número de série informado, é inválido!") }
+        await this.load(code, eventMap, event, tsPath)
+    }
 
-        } catch (error) {
-            if (error.hasOwnProperty("message")) {
+    /**
+     * Carrega o script do produto
+     * @param {string} productCode Código do produto
+     * @param {string[]} eventMap Informa ao script o mapa de eventos a serem cumpridos
+     * @param {string} event Informa ao script o evento atual
+     * @param {string | string[]} tsPath Caminho relativo dos scripts a serem carregados em relação à ESTE arquivo
+     */
+    static async load(productCode, eventMap, event, tsPath) {
+        if (typeof tsPath == "string") { tsPath = [tsPath] }
+
+        for (const path of tsPath) {
+            try {
+                window.TS = new (await import(`${path}${productCode}.js`)).default(eventMap, event)
+                return
+            } catch (error) {
+                if (error.message.includes("Failed to fetch")) { continue }
+                
                 console.warn(error.message)
                 alert(error.message)
-            } else {
-                console.warn(error)
-                alert(error)
+                location.reload()
+                return
             }
-            sessionStorage.clear()
-            location.reload()
         }
+
+        alert(`Arquivo ${productCode}.js não foi encontrado!`)
+        location.reload()
     }
+
+    /**
+     * Inicia uma instância do Rast para carregar as informações no PVI
+     * @param {string[]} eventMap Informa ao script o mapa de eventos a serem cumpridos
+     * @param {string} event Informa ao script o evento atual
+     */
+    static async rastInit(eventMap, event) {
+        sessionStorage.setItem("TestComponents", "{}") //Seta algo no sessionStorage para que o script não solicite os componentes
+
+        const rast = new RastPVI(eventMap, event)
+        const serialNumber = prompt("Informe o número de série do produto:\nEx: 1000001234567")
+        rast.SerialNumber = serialNumber
+
+        RastUtil.setValidations(RastUtil.DISABLED, RastUtil.DISABLED, RastUtil.DISABLED, RastUtil.DISABLED)
+        if (!await rast.init()) {
+            serialNumber == null || serialNumber == ""
+                ? alert("É preciso informar um número de série!")
+                : alert(`Não foi possível buscar as informações do produto com o número de série '${serialNumber}'!`)
+        }
+        RastUtil.setValidations(RastUtil.ENABLED, RastUtil.ENABLED, RastUtil.ENABLED, RastUtil.ENABLED)
+        sessionStorage.clear()
+    }
+
     static { console.log("TestScriptLoader is ready!") }
 }
